03fa176243a4eafa2b34149bbd108485a9c909f1
Julia Demura <usagiololo@gmail.com>
2026-04-25 15:16:57 +0300

    tasks is done

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,22 @@
-function filterArray(numbers, value) {
-    let numbersArr = [];
-    for (const number of numbers) {
-        if (number > value) {
-            numbersArr.push(number);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        profile.username = newName;
+    },
+    updatePlayTime(hours) {
+        profile.playTime = profile.playTime + hours;
+    },
+    getInfo() {
+        return `${profile.username} has ${profile.playTime} active hours!`;
     }
-    return numbersArr;
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
